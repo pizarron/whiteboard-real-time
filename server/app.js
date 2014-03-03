4905a9ba1c645b83910e8cbf68698501f4b18fe6
Arnold Paye <arnoldpayet@gmail.com>
2014-03-03 17:53:48 -0400

    create both chat and whiteboard sockets

--- server/app.js
+++ server/app.js
@@ -1,5 +1,17 @@
 var io = require('socket.io').listen(9090);
 
-io.sockets.on('connection', function(socket) {
-    socket.send('Welcome to whiteboard!');
+/* Socket Chat */
+this.socketChat = io.of("/chat");
+this.socketChat.on("connection", function(socket) {
+    socket.on("set_user", function(user) {
+        socket.set('user', user, function() {
+            socket.emit("user_on", user);
+        });
+    });
+});
+
+/* Socket Whiteboard */
+this.socketWBoard = io.of("/wboard");
+this.socketChat.on("connection", function(socket) {
+    socket.send("Welcome to whiteboard");
 });
