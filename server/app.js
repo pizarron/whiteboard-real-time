5c27e813a57e5c5a9f0bc23bf11e28c8ea5ae76a
Arnold Paye <arnoldpayet@gmail.com>
2014-03-04 05:30:20 -0400

    implement basic chat

--- server/app.js
+++ server/app.js
@@ -3,28 +3,21 @@ var io = require('socket.io').listen(9090);
 /* Socket Chat */
 this.socketChat = io.of("/chat");
 this.socketChat.on("connection", function(socket) {
+    socket.json.send({"user": "pizarron", "message": "Welcome to pizarron chat!"});
     
+    socket.on("message", function(message) {
+        socket.json.send(message);
+    });
+
+    socket.on("message_to_server", function(message) {
+        socket.emit("message_to_client", message);
+        message.user = "anonymous";
+        socket.broadcast.json.send(message);
+    });
 });
 
 /* Socket Whiteboard */
 this.socketWBoard = io.of("/wboard");
 this.socketWBoard.on("connection", function(socket) {
-    socket.on("set_user", function(user) {
-        socket.set("user", user, function() {
-            socket.emit("user_on", user);
-            var message = {"content": "Welcome " + user.name + "!"};
-            socket.send(message);
-        });
-    });
-    socket.on("join_course_class", function(courseClass) {
-    	socket.get("user", function(error, user) {
-    		socket.join(courseClass.id);
-    		socket.room = courseClass.id;
-    		socket.in(courseClass.id).broadcast.emit("user_entered", user);
-    	});
-    });
-    socket.on("message", function(message) {
-    	socket.in(socket.room).broadcast.send(message);
-    	socket.send(message);
-    });
+    
 });
