9808f132adbcce081b061a82ff157ed54341f109
Arnold Paye <arnoldpayet@gmail.com>
2014-03-04 00:18:53 -0400

    chat implementation

--- server/app.js
+++ server/app.js
@@ -3,15 +3,28 @@ var io = require('socket.io').listen(9090);
 /* Socket Chat */
 this.socketChat = io.of("/chat");
 this.socketChat.on("connection", function(socket) {
-    socket.on("set_user", function(user) {
-        socket.set('user', user, function() {
-            socket.emit("user_on", user);
-        });
-    });
+    
 });
 
 /* Socket Whiteboard */
 this.socketWBoard = io.of("/wboard");
-this.socketChat.on("connection", function(socket) {
-    socket.send("Welcome to whiteboard");
+this.socketWBoard.on("connection", function(socket) {
+    socket.on("set_user", function(user) {
+        socket.set("user", user, function() {
+            socket.emit("user_on", user);
+            var message = {"content": "Welcome " + user.name + "!"};
+            socket.send(message);
+        });
+    });
+    socket.on("join_course_class", function(courseClass) {
+    	socket.get("user", function(error, user) {
+    		socket.join(courseClass.id);
+    		socket.room = courseClass.id;
+    		socket.in(courseClass.id).broadcast.emit("user_entered", user);
+    	});
+    });
+    socket.on("message", function(message) {
+    	socket.in(socket.room).broadcast.send(message);
+    	socket.send(message);
+    });
 });
