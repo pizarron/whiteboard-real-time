e23383a3552993c65fe698b97ff91bd6a0ddc811
YersonKira <yerson_kira@hotmail.com>
2014-03-13 09:42:42 -0400

    rooms chat

--- server/app.js
+++ server/app.js
@@ -1,18 +1,22 @@
 var io = require('socket.io').listen(9090, { log: false });
-
 /* Socket Chat */
 this.socketChat = io.of("/chat");
 this.socketChat.on("connection", function(socket) {
-    socket.json.send({"user": "pizarron", "message": "Welcome to pizarron chat!"});
-    
-    socket.on("message", function(message) {
-        socket.json.send(message);
+    socket.on('join_chat_room', function(room){
+        socket.join(room);
     });
-
-    socket.on("message_to_server", function(message) {
-        socket.emit("message_to_client", message);
-        message.user = "anonymous";
-        socket.broadcast.json.send(message);
+    /*
+        message: {
+            room: 'aula al cual pertenece (room)',
+            user: 'Nombre de usuario (nick)',
+            message: 'Mensaje'
+        }
+     */
+    socket.on('send_message', function(message){
+        socket.broadcast.to(message.room).emit('receive_message', { user: message.user, message: message.message});
+    });
+    socket.on('leave_chat_room', function(room){
+        socket.leave(room);
     });
 });
 
@@ -23,31 +27,30 @@ this.socketWBoard.on("connection", function(socket) {
     Tanto un profesor como un estudiante lanzan este evento para unirse a un aula (room)
      */
     socket.on('join_room', function(room){
-        console.log('Join ' + room);
         socket.join(room);
+        console.log('Socket: ' + socket.id + ' joined to room: ' + room);
     });
     /*
      Evento que debe lanzar el profesor para enviar los datos de su pizarra a todos los estudiantes
      que esten en su misma aula (room)
 
+     data : {
+         room: 'room del profesor',
+         data: {
+            datos de la pizarra
+         }
+     }
      */
     socket.on('send_data', function(data){
-        console.log('Datas ' + JSON.stringify(data));
-        /*
-             data : {
-                  room: 'room del profesor',
-                  data: {
-                        datos de la pizarra
-                  }
-             }
-         */
-        socket.broadcast.to(data.room).emit('receive_datas', data.data);
+        if (data.room in rooms) {
+            console.log('Datas ' + JSON.stringify(data));
+            socket.broadcast.to(data.room).emit('receive_datas', data.data);
+        }
     });
     /*
      Tanto un profesor como un estudiante lanzan este evento para dejar un aula (room)
      */
     socket.on('leave_room', function(room){
         console.log('Leave ' + room);
-        socket.leave(room);
     });
 });
