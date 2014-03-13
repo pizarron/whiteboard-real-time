84a0abc903e40178deee95ee067f90297c6b6ab4
YersonKira <yerson_kira@hotmail.com>
2014-03-12 23:14:04 -0400

    Comments

--- server/app.js
+++ server/app.js
@@ -19,15 +19,33 @@ this.socketChat.on("connection", function(socket) {
 /* Socket Whiteboard */
 this.socketWBoard = io.of("/wboard");
 this.socketWBoard.on("connection", function(socket) {
+    /*
+    Tanto un profesor como un estudiante lanzan este evento para unirse a un aula (room)
+     */
     socket.on('join_room', function(room){
         console.log('Join ' + room);
         socket.join(room);
     });
+    /*
+     Evento que debe lanzar el profesor para enviar los datos de su pizarra a todos los estudiantes
+     que esten en su misma aula (room)
+
+     */
     socket.on('send_data', function(data){
         console.log('Datas ' + JSON.stringify(data));
+        /*
+             data : {
+                  room: 'room del profesor',
+                  data: {
+                        datos de la pizarra
+                  }
+             }
+         */
         socket.broadcast.to(data.room).emit('receive_datas', data.data);
-        //socket.in(data.room).emit('receive_datas', data.data);
     });
+    /*
+     Tanto un profesor como un estudiante lanzan este evento para dejar un aula (room)
+     */
     socket.on('leave_room', function(room){
         console.log('Leave ' + room);
         socket.leave(room);
