66a701ae1e9c21f92a6ad7466cdfa0c0bd6379c9
donkeysharp <serguimant@gmail.com>
2014-04-06 13:42:31 -0400

    PZR-1 fixed log for ip address right print format

--- server/app.js
+++ server/app.js
@@ -88,7 +88,7 @@ this.socketWBoard.on("connection", function(socket) {
     var handshake = socket.handshake;
     if (handshake) {
         var ipaddr = handshake.address;
-        myLog('[+] Connected from IP: ' + ipaddr);
+        myLog('[+] Connected from IP: ' + ipaddr.address);
     }
     socket.on('join_room', function(info){
         myLog(JSON.stringify(info));
